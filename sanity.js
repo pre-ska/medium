5e935b8a574e3fdf88c41ce75fce24cf0985bc95
preska <wolfwar@outlook.com>
2022-12-02 22:50:07 +0100

    post page, dynamic slug

--- sanity.js
+++ sanity.js
@@ -1,12 +1,9 @@
-import {
-  createImageUrlBuilder,
-  createCurrentUserHook,
-  createClient,
-} from 'next-sanity';
+import { createCurrentUserHook, createClient } from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: '2021-08-31',
   useCdn: process.env.NODE_ENV === 'production',
 };
@@ -18,4 +15,4 @@ export const sanityClient = createClient(config);
 export const urlFor = (source) => createImageUrlBuilder(config).image(source);
 
 // Helper for using current logged in account
-export const useCurrentUser = createCurrentUserHook(config);
+// export const useCurrentUser = createCurrentUserHook(config);
